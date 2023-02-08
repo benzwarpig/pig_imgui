6f72b98c31ed30e860202afed1375b14e3dce265
benzwarpig <550791785@qq.com>
2023-02-08 18:23:25 +0800

    ✨ feat(implot): 新增implot库

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <imgui/imgui.h>
+#include <imgui/implot.h>
+#include <imgui/implot_internal.h>
+
+int main(void)
+{
+    ImPlot::ShowDemoWindow();
+    std::cout << "hello world" << std::endl;
+}
